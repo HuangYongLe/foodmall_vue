3a6374ffea1afc42a731c3fde6af39df6bce5ddc
hyl <yongle_h@126.com>
2019-04-27 14:50:09 +0800

    shopcart about finish

---
+++ src/common/animations/FoldAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'FoldAnimation'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .v-enter-to, .v-leave
+    transform translate3d(0, -100%, 0)
+  .v-enter, .v-leave-to
+    transform translate3d(0, 0, 0)
+  .v-enter-active, .v-leave-active
+    transition all .2s
+</style>
--- src/components/shopcart/Shopcart.vue
+++ src/components/shopcart/Shopcart.vue
@@ -1,27 +1,57 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'highlight': totalCount > 0}">
-            <span class="iconfont shopcart-icon">&#xe6b8;</span>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highlight': totalCount > 0}">
+              <span class="iconfont shopcart-icon">&#xe6b8;</span>
+            </div>
+            <div class="num" v-show="totalCount > 0">{{totalCount}}</div>
           </div>
-          <div class="num" v-show="totalCount > 0">{{totalCount}}</div>
+          <div class="price" :class="{'highlight': totalPrice > 0}">{{totalPrice | currency('￥', 0)}}</div>
+          <div class="desc">另需配送费{{deliveryPrice | currency('￥', 0)}}元</div>
         </div>
-        <div class="price" :class="{'highlight': totalPrice > 0}">{{totalPrice | currency('￥', 0)}}</div>
-        <div class="desc">另需配送费{{deliveryPrice | currency('￥', 0)}}元</div>
-      </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">
-          {{payDesc}}
+        <div class="content-right" @click.stop="pay">
+          <div class="pay" :class="payClass">
+            {{payDesc}}
+          </div>
         </div>
       </div>
+      <fold-animation>
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="empty">清空</span>
+          </div>
+          <div class="list-content" ref="shopcartlist">
+            <ul>
+              <li class="food" v-for="(food, index) in selectFoods" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>{{food.price * food.count | currency('￥', 0)}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cart-control :food="food"></cart-control>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </fold-animation>
     </div>
+    <fade-animation>
+      <div class="list-mask" v-show="listShow" @click="hideList"></div>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import {currency} from '@/util/currency'
+import BScroll from 'better-scroll'
+import CartControl from '@/components/cartcontrol/CartControl'
+import FoldAnimation from '@/common/animations/FoldAnimation'
+import FadeAnimation from '@/common/animations/FadeAnimation'
 export default {
   name: 'Shopcart',
   props: {
@@ -40,7 +70,25 @@ export default {
       default: 0
     }
   },
+  data () {
+    return {
+      fold: true
+    }
+  },
+  components: {
+    CartControl,
+    FoldAnimation,
+    FadeAnimation
+  },
   computed: {
+    listShow () {
+      if (!this.totalCount) {
+        // this.fold = true
+        return false
+      }
+      let show = !this.fold
+      return show
+    },
     totalPrice () {
       let total = 0
       this.selectFoods.forEach((food) => {
@@ -72,11 +120,44 @@ export default {
         return 'enough'
       }
     }
+  },
+  methods: {
+    pay () {
+      if (this.totalPrice < this.minPrice) {
+        return
+      }
+      window.alert(`支付${this.totalPrice}元`)
+    },
+    hideList () {
+      this.fold = true
+    },
+    toggleList () {
+      if (!this.totalCount) {
+        return
+      }
+      this.fold = !this.fold
+    },
+    empty () {
+      this.selectFoods.forEach((food) => {
+        food.count = 0
+      })
+    },
+    _initScroll () {
+      this.listScroll = new BScroll(this.$refs.shopcartlist, {
+        click: true
+      })
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
   .shopcart
     position fixed
     left 0
@@ -164,4 +245,60 @@ export default {
           &.enough
             background #00b43c
             color #fff
+    .shopcart-list
+      position absolute
+      left 0
+      top 0
+      z-index -1
+      width 100%
+      transform translate3d(0, -100%, 0)
+      .list-header
+        height 40px
+        line-height 40px
+        padding 0 18px
+        background #f3f5f7
+        border-bottom 1px solid rgba(7, 17, 27, .1)
+        .title
+          float left
+          font-size 14px
+          color rgb(7, 17, 27)
+        .empty
+          float right
+          font-size 12px
+          color rgb(0, 160, 220)
+      .list-content
+        padding 0 18px
+        max-height 217px
+        overflow hidden
+        background #fff
+        .food
+          position relative
+          padding 12px 0
+          box-sizing border-box
+          border-1px(rgba(7, 17, 27, .1))
+          .name
+            line-height 24px
+            font-size 14px
+            color rgb(7, 17, 27)
+          .price
+            position absolute
+            right 90px
+            bottom 12px
+            line-height 24px
+            font-size 14px
+            font-weight 700
+            color rgb(240, 20, 20)
+          .cartcontrol-wrapper
+            position absolute
+            right 0
+            bottom 6px
+  .list-mask
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index 40
+    backdrop-filter blur(10px)
+    background rgba(7, 17, 27, .6)
 </style>
